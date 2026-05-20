4e1995d64d74cbbb51a0ebfce57a8dc09cbcd92b
Connor Peet <connor@peet.io>
2026-05-20 13:13:18 -0700

    Revert "Update `ws` dependency to latest 8.x release (#49)"
    This reverts commit 9e3bba5fa0ed5aa577f2929662831e3acc8331d3.

--- src/session.ts
+++ src/session.ts
@@ -2,10 +2,16 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
+import { spawn } from 'child_process';
+import duplexer3 from 'duplexer3';
+import { Agent } from 'http';
+import { Socket } from 'net';
+import { Duplex } from 'stream';
 import { URL } from 'url';
-import { WebSocket as NodeWebSocket } from 'node:http';
 import { Disposable, EventEmitter } from 'vscode';
-import { ITarget, ITargetMessage, normalizeMessage } from './target';
+import WebSocket from 'ws';
+import { IWslInfo } from './extension';
+import { ITarget } from './target';
 
 class MessageQueue<T> {
   private qOrFn: T[] | ((value: T) => void) = [];
@@ -43,10 +49,10 @@ export class Session implements Disposable {
 
   private disposed = false;
   private browserProcess?: ITarget;
-  private socket?: InstanceType<typeof NodeWebSocket>;
+  private socket?: WebSocket;
 
-  private fromSocketQueue = new MessageQueue<ITargetMessage>();
-  private fromBrowserQueue = new MessageQueue<ITargetMessage>();
+  private fromSocketQueue = new MessageQueue<WebSocket.RawData>();
+  private fromBrowserQueue = new MessageQueue<WebSocket.RawData>();
 
   constructor() {
     this.onClose(() => this.dispose());
@@ -56,10 +62,14 @@ export class Session implements Disposable {
   /**
    * Attaches the socket looping back up to js-debug.
    */
-  public attachSocket(host: string, port: number, path: string) {
+  public attachSocket(host: string, port: number, path: string, wslInfo?: IWslInfo) {
     const url = new URL(`ws://${host}:${port}${path}`);
     const deadline = Date.now() + 5000;
-    this.attachSocketLoop(url, deadline);
+    if (wslInfo) {
+      this.attachSocketWsl(url, wslInfo, deadline);
+    } else {
+      this.attachSocketLoop(url, deadline);
+    }
   }
 
   /**
@@ -89,44 +99,104 @@ export class Session implements Disposable {
     }
   }
 
+  private attachSocketWsl(url: URL, wslInfo: IWslInfo, deadline: number) {
+    const agent = new Agent();
+
+    // Make a fake connection that attaches to stdin/out, as in my original
+    // unrelated https://github.com/websockets/ws/issues/1944
+    //
+    // The maintainer suggested using setSocket there, but that happens after
+    // the socket is upgraded, and we want the actual HTTP request and upgrade
+    // to happen on these pipes.
+    //
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (agent as any).createConnection = (
+      _options: unknown,
+      callback: (err?: Error | null, stream?: Socket) => void,
+    ) => {
+      const process = spawn('wsl.exe', [
+        '-d',
+        wslInfo.distro,
+        '-u',
+        wslInfo.user,
+        '--',
+        wslInfo.execPath,
+        '-e',
+        `'s=net.connect(${url.port});s.pipe(process.stdout);process.stdin.pipe(s)'`,
+      ]);
+
+      process.on('error', callback);
+
+      process.on('spawn', () => {
+        callback(null, makeNetSocketFromDuplexStream(duplexer3(process.stdin, process.stdout)));
+      });
+    };
+
+    // intentionally don't perMessageDeflate here, since we're local in wsl
+    const ws = new WebSocket(url, { agent });
+    this.setupSocket(ws, url, deadline);
+  }
+
   private attachSocketLoop(url: URL, deadline: number) {
     if (this.disposed) {
       return;
     }
 
-    const socket = new NodeWebSocket(url);
-    socket.binaryType = 'arraybuffer';
+    const socket = new WebSocket(url, { perMessageDeflate: true });
     this.setupSocket(socket, url, deadline);
   }
 
-  private setupSocket(socket: InstanceType<typeof NodeWebSocket>, url: URL, deadline: number) {
-    socket.addEventListener('open', () => {
+  private setupSocket(socket: WebSocket, url: URL, deadline: number) {
+    socket.on('open', () => {
       if (this.disposed) {
         socket.close();
         return;
       }
 
       this.socket = socket;
-      this.socket.addEventListener('close', () => this.closeEmitter.fire());
-      this.socket.addEventListener('message', (event: unknown) =>
-        this.fromSocketQueue.push(normalizeMessage((event as { data: unknown }).data)),
-      );
+      this.socket.on('close', () => this.closeEmitter.fire());
+      this.socket.on('message', data => this.fromSocketQueue.push(data));
       this.fromBrowserQueue.connect(data => socket.send(data));
     });
 
-    socket.addEventListener('error', (event: unknown) => {
-      const err =
-        typeof event === 'object' &&
-        event !== null &&
-        'error' in event &&
-        (event as { error?: unknown }).error instanceof Error
-          ? (event as { error: Error }).error
-          : undefined;
+    socket.on('error', err => {
       if (this.socket === socket || Date.now() > deadline) {
-        this.errorEmitter.fire(err ?? new Error(`Error connecting websocket to ${url.toString()}`));
+        this.errorEmitter.fire(err);
       } else {
         setTimeout(() => this.attachSocketLoop(url, deadline), 100);
       }
     });
   }
 }
+
+const makeNetSocketFromDuplexStream = (stream: Duplex): Socket => {
+  const cast = stream as Socket;
+  const patched: { [K in keyof Omit<Socket, keyof Duplex>]: Socket[K] } = {
+    bufferSize: 0,
+    bytesRead: 0,
+    bytesWritten: 0,
+    connecting: false,
+    localAddress: '127.0.0.1',
+    localPort: 1,
+    remoteAddress: '127.0.0.1',
+    remoteFamily: 'tcp',
+    remotePort: 1,
+    address: () => ({ address: '127.0.0.1', family: 'tcp', port: 1 }),
+    unref: () => cast,
+    ref: () => cast,
+    connect: (_port: unknown, _host?: unknown, connectionListener?: () => void) => {
+      if (connectionListener) {
+        setImmediate(connectionListener);
+      }
+      return cast;
+    },
+    setKeepAlive: () => cast,
+    setNoDelay: () => cast,
+    setTimeout: (_timeout: number, callback?: () => void) => {
+      callback?.();
+      return cast;
+    },
+  };
+
+  return Object.assign(stream, patched) as Socket;
+};
